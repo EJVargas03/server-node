b735a1151b6e7ce0198efdd0181630e6976101a1
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-10-16 19:14:54 -0500

    Basic Node Complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 //console.log('Hi')
 //console.log('Hello There!')
 
-import { multiplication } from './maths'
+//import { multiplication } from './maths'
+//console.log(multiplication(3, 5))
 
-console.log(multiplication(3, 5))
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.get('/',(req, res) => {
+    res.send({ msg: 'Hello There This is Your PC' })
+})
+
+app.listen(port)
